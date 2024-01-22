2eb2077400f4f794d023f5039b53eb588591ef7d
Camilo Saavedra <ci.saavedras@gmail.com>
2024-01-21 23:39:12 -0300

    Create Favorite List View Controller

---
+++ GHFollowers/FavoriteListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteListVC.swift
+//  GHFollowers
+//
+//  Created by Camilo Saavedra Salas on 11-01-24.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        
+    }
+
+
+}
